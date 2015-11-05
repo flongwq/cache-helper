b84b513396c9273324cd60b9640ad605a58b50c4
flongwq <flong_wq@163.com>
2015-11-05 17:27:46 +0800

    增加key的前缀为MeilaCache_Meigou_

--- cachehelper/src/main/java/com/meila/meigou/cachehelper/MeilaCacheType.java
+++ cachehelper/src/main/java/com/meila/meigou/cachehelper/MeilaCacheType.java
@@ -8,21 +8,21 @@ package com.meila.meigou.cachehelper;
  *
  */
 public enum MeilaCacheType {
-	None, Product, Seller, Comment;
+    None, Product, Seller, Comment;
 
-	public String getPrefix() {
-		String defaultPrefix = "MeilaCache_";
-		switch (this) {
-		case None:
-			return defaultPrefix;
-		case Product:
-			return defaultPrefix + "Product_";
-		case Seller:
-			return defaultPrefix + "Seller_";
-		case Comment:
-			return defaultPrefix + "Comment_";
-		default:
-			return defaultPrefix;
-		}
-	}
+    public String getPrefix() {
+        String defaultPrefix = "MeilaCache_Meigou_";
+        switch (this) {
+            case None:
+                return defaultPrefix;
+            case Product:
+                return defaultPrefix + "Product_";
+            case Seller:
+                return defaultPrefix + "Seller_";
+            case Comment:
+                return defaultPrefix + "Comment_";
+            default:
+                return defaultPrefix;
+        }
+    }
 }
